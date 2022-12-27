2b54078d402193b8d563e7203370fc82f096bb52
MIKERCD <michaelketemarcd.2014@gmail.com>
2022-12-27 19:04:41 +0300

    Initial Commit

---
+++ githubdemo/githubdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubdemoApp.swift
+//  githubdemo
+//
+//  Created by test on 27/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct githubdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
